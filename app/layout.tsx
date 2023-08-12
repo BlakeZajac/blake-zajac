4bb33f5457a0ad2f7c84168a03ad0b3187314766
BlakeZajac <contact@blakezajac.com>
2023-08-12 21:32:31 +1000

    Revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import "./globals.css";
 import type { Metadata } from "next";
 
 import SiteHeader from "@/components/site-header";
-import { usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Blake Zajac",
@@ -16,13 +13,10 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const isSanity = pathname.includes("/sanity");
-
   return (
     <html lang="en">
       <body className="bg-light">
-        {!isSanity && <SiteHeader />}
+        <SiteHeader />
         {children}
       </body>
     </html>
