ea1dbff389d5a556caa3d04a86da4774fecbc180
BlakeZajac <contact@blakezajac.com>
2023-08-13 21:03:53 +1000

    Fix compile errors

--- components/content.tsx
+++ components/content.tsx
@@ -1,51 +1,51 @@
-"use client";
+// "use client";
 
-interface ContentProps {}
+// interface ContentProps {}
 
-const Content = () => {
-  return (
-    <div>
-      <p>
-        G'day, I'm Blake Zajac, a front end developer and UX designer proudly
-        based in Newcastle, Australia - your creative companion in the digital
-        realm.
-      </p>
+// const Content = () => {
+//   return (
+//     <div>
+//       <p>
+//         G'day, I'm Blake Zajac, a front end developer and UX designer proudly
+//         based in Newcastle, Australia - your creative companion in the digital
+//         realm.
+//       </p>
 
-      <p>
-        By day, I'm deep into the world of code, sculpting WordPress websites
-        with PHP finesse and immersing myself in the intricacies of React and
-        Next.js when the sun sets. Crafting digital experiences that find their
-        home on the web is where my heart lies.
-      </p>
+//       <p>
+//         By day, I'm deep into the world of code, sculpting WordPress websites
+//         with PHP finesse and immersing myself in the intricacies of React and
+//         Next.js when the sun sets. Crafting digital experiences that find their
+//         home on the web is where my heart lies.
+//       </p>
 
-      <p>
-        But beyond the screen, my interests reflect the diverse tapestry of
-        life. Photography is my escape, as I capture fleeting moments through
-        the lens, finding beauty in every frame. The piano is my muse, where I
-        lose myself in the melodies of Chopin and Erik Satie, a harmony that
-        echoes even in my coding endeavors.
-      </p>
+//       <p>
+//         But beyond the screen, my interests reflect the diverse tapestry of
+//         life. Photography is my escape, as I capture fleeting moments through
+//         the lens, finding beauty in every frame. The piano is my muse, where I
+//         lose myself in the melodies of Chopin and Erik Satie, a harmony that
+//         echoes even in my coding endeavors.
+//       </p>
 
-      <p>
-        As a self-proclaimed chess nerd, I've mastered the art of strategy and
-        tactics, reveling in the challenge of each move on the checkered
-        battlefield.
-      </p>
+//       <p>
+//         As a self-proclaimed chess nerd, I've mastered the art of strategy and
+//         tactics, reveling in the challenge of each move on the checkered
+//         battlefield.
+//       </p>
 
-      <p>
-        My journey into the tech world began as a web and UX designer, a path I
-        trod for three enriching years. Over time, my love for design
-        harmoniously merged with my coding prowess, propelling me into the
-        captivating domain of front end development. Every line of code, every
-        pixel perfected, is a testament to my journey of growth and innovation.
-      </p>
+//       <p>
+//         My journey into the tech world began as a web and UX designer, a path I
+//         trod for three enriching years. Over time, my love for design
+//         harmoniously merged with my coding prowess, propelling me into the
+//         captivating domain of front end development. Every line of code, every
+//         pixel perfected, is a testament to my journey of growth and innovation.
+//       </p>
 
-      <p>
-        So, whether I'm crafting lines of code, composing on the piano keys,
-        capturing moments through my camera lens, or plotting my next chess
-        masterpiece, I'm Blake Zajac - your guide in the world of web, design,
-        and beyond.
-      </p>
-    </div>
-  );
-};
+//       <p>
+//         So, whether I'm crafting lines of code, composing on the piano keys,
+//         capturing moments through my camera lens, or plotting my next chess
+//         masterpiece, I'm Blake Zajac - your guide in the world of web, design,
+//         and beyond.
+//       </p>
+//     </div>
+//   );
+// };
