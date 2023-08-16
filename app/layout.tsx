2ad2cad8e93f0048fa78585c44348474b38fa991
Blake Zajac <contact@blakezajac.com>
2023-08-16 17:03:07 +1000

    Fix runtime errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,17 @@
-"use client";
-
 import "./globals.css";
 import type { Metadata } from "next";
-import Lenis from "@studio-freight/lenis";
 import SiteHeader from "@/components/site-header";
-import { useEffect } from "react";
 
 export const metadata: Metadata = {
   title: "Blake Zajac",
   description: "Front End Developer and UX Designer",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    const lenis = new Lenis({
-      smoothWheel: true,
-      duration: 2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    });
-
-    function raf(time: any) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
     <html lang="en">
       <body className="bg-light">
