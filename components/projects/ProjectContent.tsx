1e097154a3ba43656b95f1583eb7c689dae2562b
Blake Zajac <contact@blakezajac.com>
2023-08-23 15:34:24 +1000

    Fix project array of strings rendering in lowercase

--- components/projects/ProjectContent.tsx
+++ components/projects/ProjectContent.tsx
@@ -15,18 +15,21 @@ const ProjectContent: React.FC<ProjectContentProps> = ({
   return (
     <Section>
       <Container
-        spacingLg
-        className="flex flex-col md:flex-row gap-12 md:gap-0"
+        spacingXl
+        className="flex flex-col md:flex-row gap-20 md:gap-0"
       >
-        <div className="md:w-2/5 text-black-600">
-          <ul>
+        <div className="flex flex-col gap-6 md:w-2/5 text-black-600">
+          <p className="text-uppercase text-black">Stack</p>
+
+          <ul className="flex flex-col gap-1">
             {stack.map((stackItem, index) => (
               <li key={index}>{stackItem}</li>
             ))}
           </ul>
         </div>
 
-        <div className="md:w-3/5 text-black-600 md:text-lg md:!leading[1.5] max-w-[700px]">
+        <div className="flex flex-col md:w-3/5 text-black-600 md:text-lg md:!leading[1.5] max-w-[700px]">
+          <p className="text-uppercase text-black !pb-6">Information</p>
           <PortableText value={projectContent} />
         </div>
       </Container>
